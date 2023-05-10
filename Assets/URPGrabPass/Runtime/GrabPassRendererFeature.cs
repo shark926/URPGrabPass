e3ea936772d81033de275d020da100aec2236a02
zhujiangbo <shark926@qq.com>
2023-05-10 18:28:13 +0800

    简化代码

--- Assets/URPGrabPass/Runtime/GrabPassRendererFeature.cs
+++ Assets/URPGrabPass/Runtime/GrabPassRendererFeature.cs
@@ -1,38 +1,78 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 namespace URPGrabPass.Runtime
 {
-    /// <summary>
-    ///     Renderer feature to grab color texture and render objects that use it.
-    /// </summary>
-    [Serializable]
     public class GrabPassRendererFeature : ScriptableRendererFeature
     {
         private const string DefaultShaderLightMode = "UseColorTexture";
         private const string DefaultGrabbedTextureName = "_GrabbedTexture";
 
-        [SerializeField] [Tooltip("When to grab color texture.")]
-        private GrabTiming _timing = GrabTiming.AfterTransparents;
+        [SerializeField]
+        [Tooltip("When to grab color texture.")]
+        private RenderPassEvent _timing = RenderPassEvent.AfterRenderingTransparents;
 
-        [SerializeField] [Tooltip("Texture name to use in the shader.")]
+        [SerializeField]
+        [Tooltip("Texture name to use in the shader.")]
         private string _grabbedTextureName = DefaultGrabbedTextureName;
 
         private GrabColorTexturePass _grabColorTexturePass;
 
-
         public override void Create()
         {
-            _grabColorTexturePass = new GrabColorTexturePass(_timing, _grabbedTextureName);
+            _grabColorTexturePass = new GrabColorTexturePass(_grabbedTextureName);
+            _grabColorTexturePass.renderPassEvent = _timing;
         }
 
         public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
         {
             _grabColorTexturePass.BeforeEnqueue(renderer);
             renderer.EnqueuePass(_grabColorTexturePass);
+        }
+
+        public class GrabColorTexturePass : ScriptableRenderPass
+        {
+            private const string GrabbedTextureIdentifier = "_GrabbedTexture";
+
+            private readonly RTHandle _grabbedTextureHandle;
+
+            private readonly int _grabbedTexturePropertyId;
+
+            private ScriptableRenderer _renderer;
+
+            public GrabColorTexturePass(string grabbedTextureName)
+            {
+
+                //_grabbedTextureHandle = RTHandles.Alloc(grabbedTextureName);
+                _grabbedTexturePropertyId = Shader.PropertyToID(grabbedTextureName);
+            }
+
+            public void BeforeEnqueue(ScriptableRenderer renderer)
+            {
+                _renderer = renderer;
+            }
+
+            public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+            {
+                cmd.GetTemporaryRT(_grabbedTexturePropertyId, cameraTextureDescriptor);
+
+                //cmd.SetGlobalTexture(_grabbedTexturePropertyId, _grabbedTextureHandle);
+            }
+
+            public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+            {
+                var cmd = CommandBufferPool.Get(nameof(GrabColorTexturePass));
+                cmd.Clear();
+                Blit(cmd, _renderer.cameraColorTarget, _grabbedTexturePropertyId);
+                context.ExecuteCommandBuffer(cmd);
+                CommandBufferPool.Release(cmd);
+            }
+
+            public override void FrameCleanup(CommandBuffer cmd)
+            {
+                cmd.ReleaseTemporaryRT(_grabbedTexturePropertyId);
+            }
         }
     }
 }
